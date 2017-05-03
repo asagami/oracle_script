2b8f914ed22a9a76ee804c5363576c56294193b8
Thomas Young <ryy100@gmail.com>
2017-05-03 19:47:58 +0800

    28

--- oracle.sh
+++ oracle.sh
@@ -11,17 +11,33 @@ if [ `cat /proc/meminfo | grep MemTotal | awk '{print $2}'` -lt 1048576 ];then
     echo  -e "\n\e[1;33m Memory Small \e[0m"
     exit 1
 else
-    echo -e "\n\e[1;36m Memory checked PASS \e[0m"
+  echo -e "\n\e[1;36m Memory checked PASS \e[0m"
 fi
 }
 
 
 
+
 #add group and user
- /usr/sbin/groupadd oinstall
- /usr/sbin/groupadd dba
- /usr/sbin/useradd -g oinstall -G dba oracle
- passwd oracle
+useradd(){
+  if [[ `grep "oracle" /etc/passwd` != "" ]];then
+        userdel -r oracle
+        fi
+  if [[ `grep "oinstall" /etc/group` = "" ]];then   
+        /usr/sbin/groupadd oinstall
+        fi
+  if [[ `grep "dba" /etc/group` = "" ]];then
+        /usr/sbin/groupadd dba
+        fi
+    useradd oracle -g oinstall -G dba && echo $1 |passwd oracle --stdin
+
+    if [ $? -eq 0 ];then
+        echo -e "\n\e[1;36m oracle's password updated successfully  --- OK! \e[0m"
+    else
+        echo -e "\n\e[1;31m oracle's password set faild.   --- NO!\e[0m"
+    fi
+}
+
 
 #add configuration
 kernelset()
@@ -44,6 +60,8 @@ EOF
         fi
 /sbin/sysctl -p
 }
+
+
 oralimit()
 {
 echo 'oracle soft nproc 2047
@@ -55,8 +73,25 @@ oracle hard nofile 65536' >> /etc/security/limits.conf
     fi
 cat $LIMITS | grep '^o'
 }
-echo '/etc/security/limits.conf' >> /etc/pam.d/login
 
+
+setlogin()
+{
+    cp $PAM{,.ora_bak} && cat <<EOF >>$PAM
+session     required    pam_limits.so
+EOF
+    if [ $? -eq 0 ];then
+        echo -e "\n\e[1;36m  $PAM updated successfully ... OK! \e[0m"
+    fi
+#echo '/etc/security/limits.conf' >> /etc/pam.d/login
+
+kernelset
+setlogin
+oralimit
+setlogin
+useradd
+
+#oracle
 mkdir -p /oracle/oraInventory
 chown -R oracle:oinstall /oracle/
 chmod -R 775 /oracle/
@@ -67,6 +102,7 @@ echo 'export ORACLE_BASE=/oracle
 export ORACLE_HOME=$ORACLE_BASE/oracle
 export ORACLE_SID=orcl
 export PATH=$ORACLE_HOME/bin:$PATH:$HOME/bin' >> /home/oracle/.bash_profile
+
 
 #exec
 export LANG=en_US
