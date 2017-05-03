6cc405a017f4755008ccb1e58b408284826107b2
Thomas Young <ryy100@gmail.com>
2017-05-03 15:34:49 +0800

    weblogic

--- oracle.sh
+++ oracle.sh
@@ -1,7 +1,10 @@
+#add group and user
  /usr/sbin/groupadd oinstall
  /usr/sbin/groupadd dba
  /usr/sbin/useradd -g oinstall -G dba oracle
  passwd oracle
+
+#add configuration
 
 echo 'fs.aio-max-nr = 1048576
 fs.file-max = 6815744
@@ -13,4 +16,24 @@ net.ipv4.ip_local_port_range = 9000 65500
 net.core.rmem_default = 262144
 net.core.rmem_max = 4194304
 net.core.wmem_default = 262144
-net.core.wmem_max = 1048586' >> /etc/sysctl.conf 
+net.core.wmem_max = 1048586' >> /etc/sysctl.conf
+
+/sbin/sysctl -ps
+
+echo 'oracle soft nproc 2047
+oracle hard nproc 16384
+oracle soft nofile 1024
+oracle hard nofile 65536' >> /etc/security/limits.conf 
+
+echo '/etc/security/limits.conf' >> /etc/pam.d/login
+
+mkdir -p /oracle/oraInventory
+chown -R oracle:oinstall /oracle/
+chmod -R 775 /oracle/
+
+su - oracle
+
+echo 'export ORACLE_BASE=/oracle
+export ORACLE_HOME=$ORACLE_BASE/oracle
+export ORACLE_SID=orcl
+export PATH=$ORACLE_HOME/bin:$PATH:$HOME/bin' >> /home/oracle/.bash_profile
